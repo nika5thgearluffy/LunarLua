a79c243630a7f2fb1070e66fd616fa759a177741
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-04-15 22:11:22 -0600

    Fix extra episode blank bug when calling "Episode.get()"

--- LunaDll/LuaMain/LuaProxyComponent/LuaProxyGlobalFunctions/LuaProxyGlobalFuncEpisode.cpp
+++ LunaDll/LuaMain/LuaProxyComponent/LuaProxyGlobalFunctions/LuaProxyGlobalFuncEpisode.cpp
@@ -20,7 +20,7 @@ static luabind::object getAllEpisodes(lua_State *L)
     {
         size_t counter = 0;
 
-        for (int i = 0; i <= gEpisodeMain.GetEpisodeCount(); i++)
+        for (int i = 0; i <= gEpisodeMain.GetEpisodeCount() - 1; i++)
         {
             luabind::object e = luabind::newtable(L);
             // Episode name
