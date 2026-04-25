cd15f8fb5bd8969108c68f3aa50c83210a3ccf82
The Sun God: Nika <219582684+nika5thgearluffy@users.noreply.github.com>
2026-04-25 03:20:05 -0600

    Increase NPC max ID to 2000

--- LunaDll/SMBXInternal/NPCs.h
+++ LunaDll/SMBXInternal/NPCs.h
@@ -595,7 +595,7 @@ namespace NPC {
     void SetSemisolidCollidingFlyType(unsigned int flyType, bool shouldCollide);
 
     // NOTE: This is the value we are patching it to. Originally 300
-    static const short MAX_ID = 1000;
+    static const short MAX_ID = 2000;
 }
 
 #endif
